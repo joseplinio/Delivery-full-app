cd5b3a2364a00b916211237e8afa264ecef2196c
José Plinio Ripardo Rocha <joseplinio@proton.me>
2026-02-24 18:48:48 -0300

    add the methods for the userRepo
    some stuffs on it will change, as for example the test, but repository i
    think it's ok

--- backend/src/adapters/spi/db/repositories/userRepository/user.repository.spec.ts
+++ backend/src/adapters/spi/db/repositories/userRepository/user.repository.spec.ts
@@ -1,18 +1,18 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { AuthService } from './auth.service';
+import { Test, TestingModule } from "@nestjs/testing"
+import { UserRepository } from "./user.repository"
 
-describe('AuthService', () => {
-  let service: AuthService;
+describe("AuthService", () => {
+  let service: UserRepository
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
-    }).compile();
+      providers: [UserRepository],
+    }).compile()
 
-    service = module.get<AuthService>(AuthService);
-  });
+    service = module.get<UserRepository>(UserRepository)
+  })
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
-});
+  it("should be defined", () => {
+    expect(service).toBeDefined()
+  })
+})
--- backend/src/adapters/spi/db/repositories/userRepository/user.repository.ts
+++ backend/src/adapters/spi/db/repositories/userRepository/user.repository.ts
@@ -1,16 +1,58 @@
 import { Injectable } from "@nestjs/common"
-import { IUserRepository } from "src/aplication/interfaces/repositories/I.user.repository"
+import { IUserRepository } from "src/application/interfaces/repositories/I.user.repository"
+import { PrismaService } from "src/application/services/prisma/prisma.service"
 import { UserEntity } from "src/entities/user.entity"
 
 @Injectable()
-export class UserRepository implements IUserRepository<UserEntity, any> {
-  async create(body: UserEntity): Promise<any> { }
+export class UserRepository implements IUserRepository<UserEntity, UserEntity> {
+  constructor(private prisma: PrismaService) { }
 
-  async findAll(): Promise<any> { }
+  async create(body: UserEntity): Promise<UserEntity | null> {
+    try {
+      const createResult = await this.prisma.user.create({ body })
+      return createResult
+    } catch (err) {
+      console.log(err)
+      throw new Error("some error")
+    }
+  }
 
-  async findOne(id: string): Promise<any> { }
+  async findAll(): Promise<[UserEntity] | null> {
+    try {
+      const findAllResult = await this.prisma.post.findAll()
+      return findAllResult
+    } catch (err) {
+      console.log(err)
+      throw new Error("somthing")
+    }
+  }
 
-  async update(id: string): Promise<any> { }
+  async findOne(id: string): Promise<UserEntity | null> {
+    try {
+      const findOneResult = await this.prisma.post.findOne({ where: id })
+      return findOneResult
+    } catch (err) {
+      console.log(err)
+      throw new Error("somthing")
+    }
+  }
 
-  async remove(id: number): Promise<any> { }
+  async update(id: string, data: object): Promise<UserEntity> {
+    try {
+      const updateResult = await this.prisma.post.update({ data, id })
+      return updateResult
+    } catch (err) {
+      console.log(err)
+      throw new Error("somthing")
+    }
+  }
+  async remove(id: number): Promise<void> {
+    try {
+      const removeResult = await this.prisma.post.delete({ id })
+      return removeResult
+    } catch (err) {
+      console.log(err)
+      throw new Error("somthing")
+    }
+  }
 }
