1fac8b5394c1a172bf2077c6cc7cff7ae2ea0c4e
José Plinio Ripardo Rocha <joseplinio@proton.me>
2026-03-27 18:10:58 -0300

    improving the logic on the authController | moved the module into mudule/

--- backend/src/adapters/api/controller/auth/auth.controller.ts
+++ backend/src/adapters/api/controller/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Post, Body } from "@nestjs/common"
-import { SignInDto } from "src/application/interfaces/dto/sign-in-dto"
-import { SignUpDto } from "src/application/interfaces/dto/sign-up-dto"
+import { SignInDto } from "src/application/interfaces/dto/sign.in.dto"
+import { SignUpDto } from "src/application/interfaces/dto/sign.up.dto"
+import { DtoValidatorService } from "src/application/services/dto/dto.validator.service"
 import { SignInCase } from "src/use_case/auth/sign.in.case"
 import { SignUpCase } from "src/use_case/auth/sign.up.case"
 
@@ -9,17 +10,36 @@ export class AuthController {
 	constructor(
 		private readonly signUpCase: SignUpCase,
 		private readonly signInCase: SignInCase,
+		private readonly dtoValidatior: DtoValidatorService,
 	) {}
 
 	@Post("signup")
-	async signUp(@Body() signUpDto: SignUpDto) {
-		const signUpResult = await this.signUpCase.handler(signUpDto)
-		return signUpResult
+	async signUp(@Body() signUpBody: SignUpDto) {
+		try {
+			const signUpDto = await this.dtoValidatior.valideDto<SignUpDto>(
+				SignUpDto,
+				signUpBody,
+			)
+			const signUpResult = await this.signUpCase.handler(signUpDto)
+
+			return signUpResult
+		} catch (err) {
+			throw err
+		}
 	}
 
 	@Post("signin")
-	async signIn(@Body() signInDto: SignInDto) {
-		const signInResult = await this.signInCase.handler(signInDto)
-		return signInResult
+	async signIn(@Body() signInBody: SignInDto) {
+		try {
+			const signInDto = await this.dtoValidatior.valideDto<SignInDto>(
+				SignInDto,
+				signInBody,
+			)
+			const signInResult = await this.signInCase.handler(signInDto)
+
+			return signInResult
+		} catch (err) {
+			throw err
+		}
 	}
 }
