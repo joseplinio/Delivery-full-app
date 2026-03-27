412a96556aaaacf5a9c1cd419d9baf90739271a0
José Plinio Ripardo Rocha <joseplinio@proton.me>
2026-03-27 18:14:25 -0300

    all methods finished and working

--- backend/src/adapters/spi/db/repositories/userRepository/user.repository.ts
+++ backend/src/adapters/spi/db/repositories/userRepository/user.repository.ts
@@ -1,58 +1,55 @@
 import { Injectable } from "@nestjs/common"
 import { IUserRepository } from "src/application/interfaces/repositories/I.user.repository"
 import { PrismaService } from "src/application/services/prisma/prisma.service"
-import { UserEntity } from "src/entities/user.entity"
+import { UserEntity } from "src/entities/user/user.entity"
 
 @Injectable()
 export class UserRepository implements IUserRepository<UserEntity, UserEntity> {
-  constructor(private prisma: PrismaService) { }
+	constructor(private prisma: PrismaService) {}
 
-  async create(body: UserEntity): Promise<UserEntity | null> {
-    try {
-      const createResult = await this.prisma.user.create({ body })
-      return createResult
-    } catch (err) {
-      console.log(err)
-      throw new Error("some error")
-    }
-  }
+	async create(body: UserEntity): Promise<void> {
+		try {
+			await this.prisma.user.create({ data: { ...body } })
+		} catch (err) {
+			throw err
+		}
+	}
 
-  async findAll(): Promise<[UserEntity] | null> {
-    try {
-      const findAllResult = await this.prisma.post.findAll()
-      return findAllResult
-    } catch (err) {
-      console.log(err)
-      throw new Error("somthing")
-    }
-  }
+	async findAll(): Promise<UserEntity[] | null> {
+		try {
+			const findAllResult = await this.prisma.user.findMany()
+			return findAllResult
+		} catch (err) {
+			throw err
+		}
+	}
 
-  async findOne(id: string): Promise<UserEntity | null> {
-    try {
-      const findOneResult = await this.prisma.post.findOne({ where: id })
-      return findOneResult
-    } catch (err) {
-      console.log(err)
-      throw new Error("somthing")
-    }
-  }
+	async findOne(id: string): Promise<UserEntity | null> {
+		try {
+			const findOneResult = await this.prisma.user.findUnique({ where: { id } })
+			return findOneResult
+		} catch (err) {
+			throw err
+		}
+	}
 
-  async update(id: string, data: object): Promise<UserEntity> {
-    try {
-      const updateResult = await this.prisma.post.update({ data, id })
-      return updateResult
-    } catch (err) {
-      console.log(err)
-      throw new Error("somthing")
-    }
-  }
-  async remove(id: number): Promise<void> {
-    try {
-      const removeResult = await this.prisma.post.delete({ id })
-      return removeResult
-    } catch (err) {
-      console.log(err)
-      throw new Error("somthing")
-    }
-  }
+	async update(id: string, data: object): Promise<UserEntity> {
+		try {
+			const updateResult = await this.prisma.user.update({
+				where: { id },
+				data: { ...data },
+			})
+			return updateResult
+		} catch (err) {
+			throw err
+		}
+	}
+
+	async remove(id: string): Promise<void> {
+		try {
+			await this.prisma.user.delete({ where: { id } })
+		} catch (err) {
+			throw err
+		}
+	}
 }
