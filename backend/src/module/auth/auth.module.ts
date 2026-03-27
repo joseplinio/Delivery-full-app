5fb161645d306d41f1553dfa873e5cd906d15931
José Plinio Ripardo Rocha <joseplinio@proton.me>
2026-03-27 18:28:20 -0300

    only organizing and installing / using
    installing the swagger and making the main director look better

--- backend/src/module/auth/auth.module.ts
+++ backend/src/module/auth/auth.module.ts
@@ -1,7 +1,5 @@
 import { Module } from "@nestjs/common"
 import { AuthController } from "../../adapters/api/controller/auth/auth.controller"
-import { SignInCase } from "src/use_case/auth/sign.in.case"
-import { UserRepository } from "src/adapters/spi/db/repositories/userRepository/user.repository"
 import { CaseModule } from "../case/case.module"
 import { DatabaseModule } from "../db/database.module"
 import { ServiceModule } from "../service/service.module"
