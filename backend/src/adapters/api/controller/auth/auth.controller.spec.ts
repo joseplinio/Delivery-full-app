542a495c9fc33bef218ba1e5337f170b5a7e3451
José Plinio Ripardo Rocha <joseplinio@proton.me>
2026-03-30 18:12:20 -0300

    making the tests for the user/auth controller
    only there is a test one on the auth controller (singUp) the signIn is
    coming

--- backend/src/adapters/api/controller/auth/auth.controller.spec.ts
+++ backend/src/adapters/api/controller/auth/auth.controller.spec.ts
@@ -1,18 +1,58 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { AuthController } from './auth.controller';
+import { Test, TestingModule } from "@nestjs/testing"
+import { AuthController } from "./auth.controller"
+import { SignUpCase } from "src/use_case/auth/sign.up.case"
+import { SignInCase } from "src/use_case/auth/sign.in.case"
+import { SignUpDto } from "src/application/interfaces/dto/sign.up.dto"
+import { DtoValidatorService } from "src/application/services/dto/dto.validator.service"
 
-describe('AuthController', () => {
-  let controller: AuthController;
+describe("AuthController", () => {
+  let controller: AuthController
+  let dtoValidatorService: DtoValidatorService
+  let signUpCase: SignUpCase
+  let signInCase: SignInCase
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
-    }).compile();
+      providers: [
+        { provide: SignUpCase, useValue: { handler: jest.fn() } },
+        { provide: SignInCase, useValue: { handler: jest.fn() } },
+        { provide: DtoValidatorService, useValue: { valideDto: jest.fn() } },
+      ],
+    }).compile()
 
-    controller = module.get<AuthController>(AuthController);
-  });
+    controller = module.get<AuthController>(AuthController)
+    signUpCase = module.get<SignUpCase>(SignUpCase)
+    signInCase = module.get<SignInCase>(SignInCase)
+    dtoValidatorService = module.get<DtoValidatorService>(DtoValidatorService)
+  })
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
-});
+  describe("Teting auth's routers", () => {
+    describe("SingUp/", () => {
+      it("it should sign up a user", async () => {
+        const signUpBody = {
+          name: "Test da Silva",
+          email: "someone@com.com",
+          password: "hhhh11@@@UUU",
+        } as SignUpDto
+
+        const expectedResult = {}
+
+        jest
+          .spyOn(dtoValidatorService, "valideDto")
+          .mockResolvedValue(signUpBody)
+
+        jest.spyOn(signUpCase, "handler").mockResolvedValue()
+
+        const result = await controller.signUp(signUpBody)
+
+        expect(dtoValidatorService.valideDto).toHaveBeenCalledWith(
+          SignUpDto,
+          signUpBody,
+        )
+        expect(signUpCase.handler).toHaveBeenCalledWith(signUpBody)
+        expect(result).toEqual(expectedResult)
+      })
+    })
+  })
+})
