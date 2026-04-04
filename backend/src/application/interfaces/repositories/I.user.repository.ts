d118dd53753e71e58cfcce5baafd43b9b432627d
José Plinio Ripardo Rocha <joseplinio@proton.me>
2026-04-04 18:59:31 -0300

    add new findByEmail/Id
    just puting the findByEmail and the findOne now is the byId only that.

--- backend/src/application/interfaces/repositories/I.user.repository.ts
+++ backend/src/application/interfaces/repositories/I.user.repository.ts
@@ -1,7 +1,8 @@
 export interface IUserRepository<T, N> {
   create(body: T): Promise<void | null>
   findAll(): Promise<N[] | null>
-  findOne(id: string): Promise<N | null>
+  findById(id: string): Promise<N | null>
+  findByEmail(email: string): Promise<N | null>
   update(id: string, data: object): Promise<N>
   remove(id: string): Promise<void>
 }
