c9604f1820c9e05075f265efc502d72f261d3915
José Plinio Ripardo Rocha <joseplinio@proton.me>
2026-04-04 18:58:31 -0300

    login (data) / local_strategy + authgard
    i am just following the nest doc and i made the sign-in router like it's
    not done, the strategy and auth are ready to work as well

--- backend/src/adapters/api/controller/auth/auth.controller.ts
+++ backend/src/adapters/api/controller/auth/auth.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Post, Body } from "@nestjs/common"
+import { Controller, Post, Body, Req, UseGuards } from "@nestjs/common"
+import { LocalAuthGuard } from "src/adapters/spi/auth/guards/local-auth/local.auth.guard"
 import { SignInDto } from "src/application/interfaces/dto/sign.in.dto"
 import { SignUpDto } from "src/application/interfaces/dto/sign.up.dto"
 import { DtoValidatorService } from "src/application/services/dto/dto.validator.service"
@@ -7,39 +8,59 @@ import { SignUpCase } from "src/use_case/auth/sign.up.case"
 
 @Controller("auth")
 export class AuthController {
-	constructor(
-		private readonly signUpCase: SignUpCase,
-		private readonly signInCase: SignInCase,
-		private readonly dtoValidatior: DtoValidatorService,
-	) {}
+  constructor(
+    private readonly signUpCase: SignUpCase,
+    private readonly signInCase: SignInCase,
+    private readonly dtoValidatior: DtoValidatorService,
+  ) { }
 
-	@Post("signup")
-	async signUp(@Body() signUpBody: SignUpDto) {
-		try {
-			const signUpDto = await this.dtoValidatior.valideDto<SignUpDto>(
-				SignUpDto,
-				signUpBody,
-			)
-			const signUpResult = await this.signUpCase.handler(signUpDto)
+  @Post("sign-up")
+  async signUp(@Body() signUpBody: SignUpDto) {
+    try {
+      const signUpDto = await this.dtoValidatior.valideDto<SignUpDto>(
+        SignUpDto,
+        signUpBody,
+      )
+      const signUpResult = await this.signUpCase.handler(signUpDto)
 
-			return signUpResult
-		} catch (err) {
-			throw err
-		}
-	}
+      return signUpResult
+    } catch (err) {
+      throw err
+    }
+  }
 
-	@Post("signin")
-	async signIn(@Body() signInBody: SignInDto) {
-		try {
-			const signInDto = await this.dtoValidatior.valideDto<SignInDto>(
-				SignInDto,
-				signInBody,
-			)
-			const signInResult = await this.signInCase.handler(signInDto)
+  @Post("sign-in")
+  @UseGuards(LocalAuthGuard)
+  async signIn(@Body() signInBody: SignInDto) {
+    try {
+      const signInDto = await this.dtoValidatior.valideDto<SignInDto>(
+        SignInDto,
+        signInBody,
+      )
+      const singInResult = await this.signInCase.handler(signInDto)
+      return singInResult
 
-			return signInResult
-		} catch (err) {
-			throw err
-		}
-	}
+    } catch (err) {
+      throw err
+    }
+  }
+
+  @Post("sign-out")
+  @UseGuards(LocalAuthGuard)
+  async signOut(@Req() request) {
+    try {
+      // const signInDto = await this.dtoValidatior.valideDto<SignInDto>(
+      //   SignInDto,
+      //   signInBody,
+      // )
+      //
+      // const signInResult = await this.signInCase.handler(signInDto)
+      //
+      // return signInResult
+
+      return request
+    } catch (err) {
+      throw err
+    }
+  }
 }
