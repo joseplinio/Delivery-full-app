66f21dec6c35a9d7ae37902c29d606a36ed6edd9
José Plinio Ripardo Rocha <joseplinio@proton.me>
2026-04-04 19:02:30 -0300

    adding the auth_service
    the logic of the strategy is on it

---
+++ backend/src/application/services/auth/auth.service.ts
@@ -0,0 +1,31 @@
+import { Injectable, UnauthorizedException } from "@nestjs/common"
+import { UserRepository } from "src/adapters/spi/db/repositories/userRepository/user.repository"
+import { HashService } from "../hash/hash.service"
+import { UserMapper } from "src/application/mapper/user/user.mapper"
+import { JwtService } from "@nestjs/jwt"
+import { UserEntity } from "src/entities/user/user.entity"
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private readonly userRepository: UserRepository,
+    private readonly hashService: HashService,
+    private readonly userMapper: UserMapper,
+    private readonly jwtService: JwtService,
+  ) { }
+  async validateUser(email: string, pass: string): Promise<object> {
+    const user = await this.userRepository.findByEmail(email)
+
+    if (!user) throw new UnauthorizedException("User not found!")
+
+    const isPasswordMatch = await this.hashService.compareHash(
+      pass,
+      user.password,
+    )
+    if (!isPasswordMatch) throw new UnauthorizedException("Unvalid credations!")
+
+    const cleanUser = await this.userMapper.cleanUser(user)
+    return cleanUser
+  }
+
+}
