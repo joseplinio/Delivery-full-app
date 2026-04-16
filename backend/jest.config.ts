eff82e962c918dec8927634a27dd6c863838fbd9
José Plinio Ripardo Rocha <joseplinio@proton.me>
2026-04-15 21:06:28 -0300

    the rest is only some configs

--- backend/jest.config.ts
+++ backend/jest.config.ts
@@ -1,16 +1,17 @@
-import type { Config } from 'jest';
-
-const config: Config = {
-  moduleFileExtensions: ['js', 'json', 'ts'],
-  rootDir: '.',
-  testEnvironment: 'node',
-  testRegex: '.*\\.spec\\.ts$',
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  roots: ["<rootDir>/src"],
+  testMatch: ["**/__tests__/**/*.ts", "**/?(*.)+(spec|test).ts"],
   transform: {
-    '^.+\\.(t|j)s$': 'ts-jest'
+    "^.+\\.ts$": "ts-jest",
   },
   moduleNameMapper: {
-    '^src/(.*)$': '<rootDir>/src/$1'
-  }
-};
-
-export default config;
+    "^src/(.*)$": "<rootDir>/src/$1",
+  },
+  collectCoverageFrom: [
+    "src/**/*.ts",
+    "!src/**/*.spec.ts",
+    "!src/**/*.test.ts",
+  ],
+}
