c5e66f6ee36d0f747cfb13bc18395aaa8e78171a
José Plinio Ripardo Rocha <joseplinio@proton.me>
2026-04-16 17:44:01 -0300

    adding the refreshToken method | adding some style into signIn method

--- backend/src/application/services/auth/auth.service.ts
+++ backend/src/application/services/auth/auth.service.ts
@@ -3,43 +3,69 @@ import { UserRepository } from "src/adapters/spi/db/repositories/userRepository/
 import { HashService } from "../hash/hash.service"
 import { UserMapper } from "src/application/mapper/user/user.mapper"
 import { JwtService } from "@nestjs/jwt"
+import { Inject } from "@nestjs/common"
+import type { ConfigType } from "@nestjs/config"
 import { TyJwtPayload } from "src/application/types/auth.jwt.payload"
+import jwtRefreshConfig from "../jwt/config/jwt.refresh.config"
 
 @Injectable()
 export class AuthService {
-  constructor(
-    private readonly userRepository: UserRepository,
-    private readonly hashService: HashService,
-    private readonly userMapper: UserMapper,
-    private readonly jwtService: JwtService,
-  ) { }
-  async validateUser(email: string, pass: string): Promise<object> {
-    const user = await this.userRepository.findByEmail(email)
+	constructor(
+		private readonly userRepository: UserRepository,
+		private readonly hashService: HashService,
+		private readonly userMapper: UserMapper,
+		private readonly jwtService: JwtService,
+		@Inject(jwtRefreshConfig.KEY)
+		private readonly refreshJwtConfigService: ConfigType<
+			typeof jwtRefreshConfig
+		>,
+	) {}
+	async validateUser(email: string, pass: string): Promise<object> {
+		const user = await this.userRepository.findByEmail(email)
 
-    if (!user) throw new UnauthorizedException("User not found!")
+		if (!user) throw new UnauthorizedException("User not found!")
 
-    const isPasswordMatch = await this.hashService.compareHash(
-      pass,
-      user.password,
-    )
-    if (!isPasswordMatch)
-      throw new UnauthorizedException("Unvalid credantions!")
+		const isPasswordMatch = await this.hashService.compareHash(
+			pass,
+			user.password,
+		)
+		if (!isPasswordMatch)
+			throw new UnauthorizedException("Unvalid credantions!")
 
-    const cleanUser = await this.userMapper.cleanUser(user)
-    return cleanUser
-  }
+		const cleanUser = await this.userMapper.cleanUser(user)
+		return cleanUser
+	}
 
-  async signIn(userData: { email: string; password: string }): Promise<object> {
-    const user = await this.userRepository.findByEmail(userData.email)
+	async signIn(userData: { email: string; password: string }): Promise<object> {
+		const user = await this.userRepository.findByEmail(userData.email)
 
-    if (!user) throw new UnauthorizedException("User not found!")
-    const payload: TyJwtPayload = { sub: user.id }
+		if (!user) throw new UnauthorizedException("User not found!")
+		const payload: TyJwtPayload = { sub: user.id }
 
-    const signInResult = {
-      userId: user.id,
-      access_token: this.jwtService.sign(payload),
-    }
+		const accessToken = this.jwtService.sign(payload)
+		const refreshToken = this.jwtService.sign(
+			payload,
+			this.refreshJwtConfigService,
+		)
 
-    return signInResult
-  }
+		const signInResult = {
+			userId: user.id,
+			access_token: accessToken,
+			refresh_token: refreshToken,
+		}
+
+		return signInResult
+	}
+
+	async refresToken(userId: string): Promise<object> {
+		const payload: TyJwtPayload = { sub: userId }
+		const accessToken = this.jwtService.sign(payload)
+
+		const refreshTokenResult = {
+			userId: userId,
+			access_token: accessToken,
+		}
+
+		return refreshTokenResult
+	}
 }
