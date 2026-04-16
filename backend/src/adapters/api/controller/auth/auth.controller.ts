d19d9780ba6631fb84bde29fd70a2ea5ab225257
José Plinio Ripardo Rocha <joseplinio@proton.me>
2026-04-15 21:01:20 -0300

    adding the sing-in and the account routers

--- backend/src/adapters/api/controller/auth/auth.controller.ts
+++ backend/src/adapters/api/controller/auth/auth.controller.ts
@@ -1,8 +1,10 @@
-import { Controller, Post, Body, Req, UseGuards } from "@nestjs/common"
+import { Controller, Post, Body, Req, UseGuards, Get } from "@nestjs/common"
+import { JwtAuthGuard } from "src/adapters/spi/auth/guards/jwt/jwt.guard"
 import { LocalAuthGuard } from "src/adapters/spi/auth/guards/local-auth/local.auth.guard"
 import { SignInDto } from "src/application/interfaces/dto/sign.in.dto"
 import { SignUpDto } from "src/application/interfaces/dto/sign.up.dto"
 import { DtoValidatorService } from "src/application/services/dto/dto.validator.service"
+import { GetAccountCase } from "src/use_case/auth/get.account.case"
 import { SignInCase } from "src/use_case/auth/sign.in.case"
 import { SignUpCase } from "src/use_case/auth/sign.up.case"
 
@@ -11,6 +13,7 @@ export class AuthController {
   constructor(
     private readonly signUpCase: SignUpCase,
     private readonly signInCase: SignInCase,
+    private readonly getAccount: GetAccountCase,
     private readonly dtoValidatior: DtoValidatorService,
   ) { }
 
@@ -39,7 +42,6 @@ export class AuthController {
       )
       const singInResult = await this.signInCase.handler(signInDto)
       return singInResult
-
     } catch (err) {
       throw err
     }
@@ -58,7 +60,12 @@ export class AuthController {
       //
       // return signInResult
 
-      return request
+  @Get("account")
+  @UseGuards(JwtAuthGuard)
+  async account(@Req() req: any) {
+    try {
+      const getAccountResult = await this.getAccount.handler(req.user.userId)
+      return getAccountResult
     } catch (err) {
       throw err
     }
