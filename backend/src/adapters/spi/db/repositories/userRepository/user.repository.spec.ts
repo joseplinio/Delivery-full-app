c1178db3ff40be22e0b25c1151ad56062068b180
José Plinio Ripardo Rocha <joseplinio@proton.me>
2026-05-04 15:21:35 -0300

    add the create meethod's test

--- backend/src/adapters/spi/db/repositories/userRepository/user.repository.spec.ts
+++ backend/src/adapters/spi/db/repositories/userRepository/user.repository.spec.ts
@@ -1,18 +1,51 @@
 import { Test, TestingModule } from "@nestjs/testing"
 import { UserRepository } from "./user.repository"
+import type { UserEntity } from "src/entities/user/user.entity"
 
-describe("AuthService", () => {
-  let service: UserRepository
-
+// NOTE: I need to see the why my tests are thorowig error and
+// I am not sure but I think I need to poor the Prisma righ here 
+describe("UserRepository", () => {
+  let userReposiotory: UserRepository
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserRepository],
+      providers: [
+        UserRepository,
+        {
+          provide: UserRepository,
+          useValue: {
+            create: jest.fn(),
+            findAll: jest.fn(),
+            findById: jest.fn(),
+            findByEmail: jest.fn(),
+            update: jest.fn(),
+            remove: jest.fn(),
+          },
+        },
+      ],
     }).compile()
 
-    service = module.get<UserRepository>(UserRepository)
+    userReposiotory = module.get<UserRepository>(UserRepository)
   })
+  describe("Testing the userReposiotory's method", () => {
+    it("it should create a user", async () => {
+      const userData: UserEntity = {
+        id: "some-user-id",
+        name: "jose",
+        email: "joseplinio@com.com",
+        password: "hhhh11@@@UUU",
+      }
+      const expectedResult = {
+        id: "some-user-id",
+        name: "jose",
+        email: "joseplinio@com.com",
+        password: "hhhh11@@@UUU",
+      }
 
-  it("should be defined", () => {
-    expect(service).toBeDefined()
+      jest.spyOn(userReposiotory, "create").mockResolvedValue(userData)
+      const result = await userReposiotory.create(userData)
+
+      expect(userReposiotory.create).toHaveBeenCalledWith(userData)
+      expect(result).toEqual(expectedResult)
+    })
   })
 })
